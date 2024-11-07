5121b1db1e0e92b1004a7cba17a899e3e41023c6
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2024-11-07 13:55:05 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/login");
+  }
   return (
     <div>
       <h1 className="text-red h-3 w-4 bg-red-500 px-5 py-5">Hello world!</h1>
 
-      <Button variant="outline">Eaee</Button>
+      <div className="flex w-full items-center">
+        <UserButton showName />
+      </div>
     </div>
   );
 };
